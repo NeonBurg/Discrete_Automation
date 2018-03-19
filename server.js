f29fa975ad9b9138abb733b6ae105ff55437d693
Nick.Shchavrovsky <gar-gary@yandex.ru>
2018-03-19 16:19:28 +0500

    Test share project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({'extended' : 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
+
+require('./app/routes.js')(app);
+
+app.listen(3000, "192.168.0.101", function(){
+    console.log('Example app listening on port 3000 !')
+});
